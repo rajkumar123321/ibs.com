80901a6528df7082ebf4f3bdd81ef5871d5d2935
rajkumar123321 <105289339+rajkumar123321@users.noreply.github.com>
2022-05-10 17:47:22 +0530

    Add files via upload

---
+++ services.js
@@ -0,0 +1,14 @@
+    function toggleHide(){
+    
+    let btn = document.getElementById('btn')
+
+let para = document.getElementById('para')
+
+
+        if(para.style.display != 'none'){
+            para.style.display = 'none'
+        }
+        else{
+            para.style.display = 'block'
+        }
+    }
